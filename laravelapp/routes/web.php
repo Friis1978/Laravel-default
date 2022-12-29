81dfefe66e4aaadc8b07a9e9a0676e543e34b639
Rasmus Friis <friis1978@gmail.com>
2022-12-29 12:05:28 +0100

    default

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = "Code with Dary";
+
+    return view('welcome', [
+        'name' => $name
+    ]);
 });
