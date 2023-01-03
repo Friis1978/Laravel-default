19c672e2af3391c5cc511b463afc30ac28de893c
Rasmus Friis <friis1978@gmail.com>
2023-01-03 10:49:52 +0100

    Query builder functions

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,8 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
@@ -15,6 +14,64 @@ class PostsController extends Controller
      */
     public function index()
     {
+        // Select by id
+        // $posts = DB::select('SELECT * FROM posts WHERE id = :id', ['id' => 1]);
+
+        // Insert new post
+        // $posts = DB::insert('INSERT INTO posts 
+        // (title, excerpt, body, image_path, is_published, min_to_read) 
+        // VALUES(?, ?, ?, ?, ?, ?)', ['Test2', 'test', 'test', 'test', true, 1]);
+
+        // Update post
+        // $posts = DB::update('UPDATE posts set body = ? where id = ?', ['Body 3', 101]);
+
+        // Delete posts
+        // $posts = DB::delete('DELETE FROM posts where id = ?', [101]);
+
+        // Changing operations
+        // $posts = DB::table('posts')
+        //     //->distinct() // Selecting unique values
+        //     //->whereNotNull('excerpt')
+        //     //->whereNull('excerpt')
+        //     //->whereIn('min_to_read', [2, 6, 8])
+        //     //->whereNotBetween('min_to_read', [2,6])
+        //     //->whereBetween('min_to_read', [2,6])
+        //     //->where('is_published', true)
+        //     //->where('id', '>', 50)
+        //     //->orderBy('id','desc') // asc by default
+        //     //->select('min_to_read')
+        //     //->skip(10) // skip the first 10 values
+        //     //->take(5) // take 5 after
+        //     //->inRandomOrder()
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     //->where('id', 10)
+        //     // ->first();
+        //     ->find(100);
+
+        // $posts = DB::table('posts')
+        //     ->where('id', 100)
+        //     ->value('body');
+
+        // Count columns
+        // $posts = DB::table('posts')
+        //     ->where('id', '>', 50)
+        //     ->count();
+
+        // Max value
+        // $posts = DB::table('posts')
+        //     ->max('min_to_read');
+
+        // Min value
+        // $posts = DB::table('posts')
+        //     ->min('min_to_read');
+
+        // AVg value
+        $posts = DB::table('posts')
+            ->avg('min_to_read');
+            
+        dd($posts);
         return view('blog.index');
     }
 
