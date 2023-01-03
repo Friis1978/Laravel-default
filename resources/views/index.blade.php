3cd0cdce18bcd444157a6a96da89451e356dfa32
Rasmus Friis <friis1978@gmail.com>
2023-01-03 11:11:49 +0100

    install tailwind 9

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,12 @@
-<h1>
-    Home Page [Invoke]
-</h1>
-
-<a href={{ route('blog.show', ['id' => 1]) }}>Blog</a>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+      </head>
+<body>
+    <h1 class="text-3xl p-2 font-bold bg-red-700">
+        Tailwind 3.0
+    </h1>
+</body>
+</html>
